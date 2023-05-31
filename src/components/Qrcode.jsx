9f8638f4b47548b07fca61fc49c99a7f117bbbb6
Yohann Obiang <104835751+YohannObiang@users.noreply.github.com>
2023-05-31 17:02:44 +0200

    kou

--- src/components/Qrcode.jsx
+++ src/components/Qrcode.jsx
@@ -3,6 +3,27 @@ import QrReader from 'react-qr-scanner'
 import QrScanner from 'react-qr-scanner';
 
 
+function isBackCameraAvailable() {
+    if ('mediaDevices' in navigator && 'enumerateDevices' in navigator.mediaDevices) {
+      return navigator.mediaDevices.enumerateDevices()
+        .then(devices => devices.some(device => device.kind === 'videoinput' && device.label.toLowerCase().includes('back')));
+    }
+    return Promise.resolve(false);
+  }
+  
+  isBackCameraAvailable()
+    .then(isAvailable => {
+      if (isAvailable) {
+        // L'appareil photo arrière est disponible
+      } else {
+        // L'appareil photo arrière n'est pas disponible
+      }
+    })
+    .catch(error => {
+      console.error('Erreur lors de la vérification de l\'appareil photo :', error);
+    });
+  
+
 function handleScan(result) {
     if (result) {
       console.log(result.text);
@@ -14,8 +35,15 @@ function handleScan(result) {
   }
   
   function Qrcode() {
+
+    
+    const constraints = {
+        video: { facingMode: 'environment' }
+      };
+    
     return (
       <QrScanner
+        constraints={constraints}
         delay={300}
         onError={handleError}
         onScan={handleScan}
